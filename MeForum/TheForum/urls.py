3ab8582f9e69396b5e21433be22d76b54a40a8ca
begrar <mielewczyk264@gmail.com>
2023-11-16 17:24:02 +0100

    add visible posts

--- MeForum/TheForum/urls.py
+++ MeForum/TheForum/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+#from . import views
+from .views import HomeView, ArticleDetailVIew
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    #path('', views.home, name="home"),
+    path('',  HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailVIew.as_view(), name='article-details'),
 ]
--- MeForum/TheForum/views.py
+++ MeForum/TheForum/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'index.html', {})
+#def home(request):
+#   return render(request, 'index.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class ArticleDetailVIew(DetailView):
+    model = Post
+    template_name = 'article_details.html'

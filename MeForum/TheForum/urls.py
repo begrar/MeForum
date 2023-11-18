739c0ccdf86dcbda5464566471f69a99b9741d26
begrar <mielewczyk264@gmail.com>
2023-11-18 15:31:00 +0100

    add edit and update forum post

--- MeForum/TheForum/urls.py
+++ MeForum/TheForum/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailVIew, AddPostView
+from .views import HomeView, ArticleDetailVIew, AddPostView, UpdatePostView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('',  HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailVIew.as_view(), name='article-details'),
-    path('add_post/', AddPostView.as_view(), name='add_post')
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]

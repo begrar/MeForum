1e20394ec474a1144a5dfc8101098e4526164572
begrar <mielewczyk264@gmail.com>
2023-11-19 19:09:34 +0100

    add forum category navbar links

--- MeForum/TheForum/views.py
+++ MeForum/TheForum/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
@@ -10,17 +11,34 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'index.html'
+    cat_menu = Category.objects.all()
     ordering = ['-post_date']
     #ordering = ['-id']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+    
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-',' '))
     return render(request, 'category.html', {'cats':cats.title().replace('-',' '), 'category_posts':category_posts})
 
 
-class ArticleDetailVIew(DetailView):
+class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post

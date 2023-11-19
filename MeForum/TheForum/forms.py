abf03ba7a82004d21e67664241bbcaf03f28f296
begrar <mielewczyk264@gmail.com>
2023-11-19 21:43:58 +0100

    add restrict about forum post, update admin view

--- MeForum/TheForum/forms.py
+++ MeForum/TheForum/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Post, Category
+#create custom form
+from django.contrib.auth.forms import UserCreationForm
+#create custom form
 
 #choices = [('coding','codnig'), ('sports', 'sports'), ('entertainment', 'entertainment'),]
 choices = Category.objects.all().values_list('name','name')
@@ -9,6 +12,18 @@ choices_list = []
 for item in choices:
     choices_list.append(item)
 
+
+
+#create custom form
+class CustomUserCreationForm(UserCreationForm):        
+    # make fields required if desired
+    # first_name = forms.CharField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        fields = ("username", "first_name", "last_name")
+#create custom form
+
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -17,7 +32,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder Stuff',}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
             'category':forms.Select(choices=choices_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
 

8768cc42539c0bb36d51da217313019c107d268e
begrar <mielewczyk264@gmail.com>
2023-11-21 17:46:06 +0100

    add fields to registration form

--- MeForum/TheForum/views.py
+++ MeForum/TheForum/views.py
@@ -11,21 +11,22 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
+
     liked = False
+
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)
         liked = False
     else:
         post.likes.add(request.user)
         liked = True
-
     return HttpResponseRedirect(reverse('article-details', args=[str(pk)]))
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'index.html'
-    cat_menu = Category.objects.all()
+    cats = Category.objects.all()
     ordering = ['-post_date']
     #ordering = ['-id']
 

b10e4a9588882abff0a7c72199beb4b12711720b
begrar <mielewczyk264@gmail.com>
2023-11-22 17:51:41 +0100

    add edit profile page

--- MeForum/members/urls.py
+++ MeForum/members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegsterView
+from .views import UserRegsterView, UserEditView
 
 urlpatterns = [
     path('register/', UserRegsterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 ]
--- MeForum/members/views.py
+++ MeForum/members/views.py
@@ -1,6 +1,8 @@
+from typing import Any
+from django.db import models
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SingUpForm
 
@@ -8,3 +10,11 @@ class UserRegsterView(generic.CreateView):
     form_class = SingUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self,):
+        return self.request.user

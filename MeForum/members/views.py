c272d1e60c73cacdc404d9e432d397620495ce66
begrar <mielewczyk264@gmail.com>
2023-11-27 22:40:26 +0100

    add account creation page with bug

--- MeForum/members/views.py
+++ MeForum/members/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SingUpForm, EditProfileForm, PasswordChangingForm
+from .forms import SingUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
 from TheForum.models import Profile
+
+class CreateUserProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    tempalet_name = "registration/create_user_profile_page.html"
+    #fields = '__all__'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class EditProfilePageView(generic.UpdateView):
     model = Profile
